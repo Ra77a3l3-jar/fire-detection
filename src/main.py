1a258393083c7255d9db12d0399261ca6e647f52
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2026-02-16 13:49:57 +0100

    Add Yolo detection with pretrained model for fire detection
    On the cli are displayed [] if in that frame no fire was detected, and
    [{'box': (np.int64(536), np.int64(1485), np.int64(703), np.int64(1680)),
    'confidence': 0.7547518610954285}] if a fire was detected with the coord
    of the area and the confidence

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 from capture.adb_capture import ADBCapture
+from detection.yolo_detector import Detector
 import cv2
 
 cap = ADBCapture()
+detector = Detector()
 
 while True:
     frame = cap.get_frame()
 
     if frame is None:
         continue
+
+    detections = detector.detect(frame)
+
+    print(detections)
 
     cv2.imshow("test", frame)
 
