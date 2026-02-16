720839f0967fa3c31b4d3fa7747b93b11e138b81
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2026-02-16 14:19:12 +0100

    Add bounding box with confidence display

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from capture.adb_capture import ADBCapture
 from detection.yolo_detector import Detector
+from ui.render import Render
 import cv2
 
 cap = ADBCapture()
 detector = Detector()
+renderer = Render()
 
 while True:
     frame = cap.get_frame()
@@ -13,7 +15,7 @@ while True:
 
     detections = detector.detect(frame)
 
-    print(detections)
+    frame = renderer.draw(frame, detections)
 
     cv2.imshow("test", frame)
 
