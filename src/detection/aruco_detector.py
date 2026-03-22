afad1777abe4a09b43cf8ff2b4fb85e8cf5ab8b6
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2026-03-22 13:04:01 +0100

    Fix get aruco dict name

--- src/detection/aruco_detector.py
+++ src/detection/aruco_detector.py
@@ -5,7 +5,7 @@ import config
 class ARUCODetector:
 
     def __init__ (self) -> None:
-        aruco_dict = getattr(cv2, config.ARUCO_DICTIONARY_NAME)
+        aruco_dict = getattr(cv2.aruco, config.ARUCO_DICTIONARY_NAME)
         self.aruco_dict = cv2.aruco.getPredefinedDictionary(aruco_dict)
         self.parameters = cv2.aruco.DetectorParameters()
 
